8157780b6e569d6e8b92258365e84d0f73173997
wingyen <wingyen.chan23@gmail.com>
2021-03-01 14:14:31 +0100

    rework

--- bot_prototype.py
+++ bot_prototype.py
@@ -9,62 +9,58 @@ app = Flask(__name__)
 inmemory_storage = defaultdict(list)
 
 
-class Conversation(object):
+class Dialog(object):
     def __init__(
-            self, conversation_id: Text, old_conversation_events: List[Dict]
+            self, dialog_id: Text, old_events: List[Dict]
     ) -> None:
-        """Creates a conversation.
+        """Creates a dialog.
 
         Args:
-            old_conversation_events: Events which happened earlier in this conversation.
+            old_events: Events which happened earlier in this dialog.
         """
-        self.conversation_id = conversation_id
-        self.conversation_events = old_conversation_events
-        self.number_old_events = len(old_conversation_events)
+        self.dialog_id = dialog_id
+        self.dialog_events = old_events
+        self.number_old_events = len(old_events)
 
     def add_user_message(self, message: Text) -> None:
-        self.conversation_events.append({"type": "user", "message": message})
+        self.dialog_events.append({"type": "user", "message": message})
 
     def add_bot_message(self, bot_messages: Text) -> None:
-        self.conversation_events.append({"type": "bot", "message": bot_messages})
+        self.dialog_events.append({"type": "bot", "message": bot_messages})
 
     def new_events_dict(self) -> List[Dict]:
-        return self.conversation_events[self.number_old_events:]
+        return self.dialog_events[self.number_old_events:]
 
 
 @contextmanager
-def conversationPersistence(
-        conversation_id: Text,
-) -> Generator[Conversation, None, None]:
-    """Provides conversation history for a certain conversation.
+def dialog_persistence(conversation_id: Text) -> Generator[Dialog, None, None]:
+    """Provides dialog history for a certain dialog.
 
-    Saves any new events to the conversation storage when the context manager is exited.
+    Saves any new events to the dialog storage when the context manager is exited.
 
     Args:
-        conversation_id: The ID of the conversation. This is usually the same as the
+        conversation_id: The ID of the dialog. This is usually the same as the
             username.
 
     Returns:
-        Conversation from the conversation storage.
+        Conversation from the dialog storage.
     """
     old_conversation_events = inmemory_storage[conversation_id]
-    # if old_conversation_events is None:
-    #     old_conversation_events = []
-    conversation = Conversation(conversation_id, old_conversation_events)
+    conversation = Dialog(conversation_id, old_conversation_events)
 
     yield conversation
 
 
 # random jokes
-class ChuckNorrisBot:
-    def handle_message(self, message_text: Text, conversation: Conversation) -> None:
-        conversation.add_user_message(message_text)
+class ChuckNorrisRandomBot:
+    def handle_message(self, message_text: Text, dialog: Dialog) -> None:
+        dialog.add_user_message(message_text)
 
-        if len(conversation.conversation_events) <= 1:
-            conversation.add_bot_message(f"Welcome! Let me tell you a joke.")
+        if len(dialog.dialog_events) <= 1:
+            dialog.add_bot_message(f"Welcome! Let me tell you a joke.")
 
         joke = self.retrieve_joke()
-        conversation.add_bot_message(joke)
+        dialog.add_bot_message(joke)
 
     def retrieve_joke(self) -> Text:
         response = requests.get("https://api.chucknorris.io/jokes/random")
@@ -72,16 +68,16 @@ class ChuckNorrisBot:
 
 # Search for jokes
 class ChuckNorrisJokeFinderBot:
-    def handle_message(self, message_text: Text, conversation: Conversation) -> None:
-        conversation.add_user_message(message_text)
-        self.process_finder_message(message_text, conversation)
+    def handle_message(self, message_text: Text, dialog: Dialog) -> None:
+        dialog.add_user_message(message_text)
+        self.process_finder_message(message_text, dialog)
 
-    def process_finder_message(self, message_text: Text, conversation: Conversation) -> None:
+    def process_finder_message(self, message_text: Text, dialog: Dialog) -> None:
         message_size = len(message_text)
-        conversation_size = len(conversation.conversation_events)
+        conversation_size = len(dialog.dialog_events)
 
         if conversation_size <= 1:
-            conversation.add_bot_message(f"Welcome! Let me find you jokes about {message_text}")
+            dialog.add_bot_message(f"Welcome! Let me find you jokes about {message_text}")
 
         if message_size in range(3, 121):
             """If message size is not between 3 and 120, 
@@ -90,11 +86,11 @@ class ChuckNorrisJokeFinderBot:
             jokes = self.finder_retrieve_joke(message_text)
             if jokes:
                 for joke in jokes:
-                    conversation.add_bot_message(joke)
+                    dialog.add_bot_message(joke)
             else:
-                conversation.add_bot_message("Sorry! No jokes found. Try another word.")
+                dialog.add_bot_message("Sorry! No jokes found. Try another word.")
         else:
-            conversation.add_bot_message("You have an invalid input, please try again. Size must be between 3 and 120.")
+            dialog.add_bot_message("You have an invalid input, please try again. Size must be between 3 and 120.")
 
     def finder_retrieve_joke(self, message: Text) -> List:
         response = requests.get(f"https://api.chucknorris.io/jokes/search?query={message}")
@@ -113,29 +109,26 @@ class BotFactory:
     def choose_bot(self):
         if self.query_val == "jokeFinder":
             return ChuckNorrisJokeFinderBot()
-        return ChuckNorrisBot()
+        return ChuckNorrisRandomBot()
 
 
-@app.route("/user/<username>/message", methods=["POST"])
-def handle_user_message(username: Text) -> Text:
+@app.route("/user/<dialog_id>/message", methods=["POST"])
+def handle_user_message(dialog_id: Text) -> Text:
     """Returns a bot response for an incoming user message.
 
-    Args:
-        username: The username which serves as unique conversation ID.
-
-    Returns:
-        The bot's responses.
+    :param dialog_id: : The id which serves as unique dialog ID.
+        
     """
     message_text = request.json["text"]
     query_arg = request.args.get("bot_type", "")
     factory = BotFactory(query_arg)
     bot = factory.choose_bot()
 
-    with conversationPersistence(username) as conversation:
-        bot.handle_message(message_text, conversation)
+    with dialog_persistence(dialog_id) as dialog:
+        bot.handle_message(message_text, dialog)
 
         bot_responses = [
-            x["message"] for x in conversation.new_events_dict() if x["type"] == "bot"
+            x["message"] for x in dialog.new_events_dict() if x["type"] == "bot"
         ]
 
         return jsonify(bot_responses)
@@ -143,13 +136,13 @@ def handle_user_message(username: Text) -> Text:
 
 @app.route("/user/<username>/message", methods=["GET"])
 def retrieve_conversation_history(username: Text) -> Text:
-    """Returns all conversation events for a user's conversation.
+    """Returns all dialog events for a user's dialog.
 
     Args:
-        username: The username which serves as unique conversation ID.
+        username: The username which serves as unique dialog ID.
 
     Returns:
-        All events in this conversation, which includes user and bot messages.
+        All events in this dialog, which includes user and bot messages.
     """
     history = inmemory_storage[username]
     if history:
